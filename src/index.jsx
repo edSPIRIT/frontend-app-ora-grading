f681572e0dd1af48461605770d3cc47ff31f8458
milad-emami <milad.emami1025@gmail.com>
2024-12-21 15:35:55 +0330

    fix: add react-query provider

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import store from 'data/store';
 import {
@@ -19,10 +20,27 @@ import messages from './i18n';
 
 import App from './App';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // Set staleTime to 5 minutes
+      staleTime: 5 * 60 * 1000,
+      // Set cacheTime to 60 minutes
+      cacheTime: 60 * 60 * 1000,
+      // Set the retry count for queries here
+      retry: 1, // Set the retry count for queries here
+    },
+    mutations: {
+      retry: 1, // Set the retry count for mutations here
+    },
+  },
+});
 subscribe(APP_READY, () => {
   ReactDOM.render(
     <AppProvider store={store} wrapWithRouter={false}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </AppProvider>,
     document.getElementById('root'),
   );
